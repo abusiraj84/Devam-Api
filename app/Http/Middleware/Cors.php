062d69f2b04b158c6fc8f438793fe7c8edee3187
abusiraj84 <abusiraj84@gmail.com>
2020-11-23 17:53:37 +0300

    make it better

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,12 +1,14 @@
 <?php
+# File: app\Http\Middleware\CORS.php
+# Create file with below code in above location. And at the end of the file there are other instructions also. 
+# Please check. 
 
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
-class Cors
-{
+class CORS {
+
     /**
      * Handle an incoming request.
      *
@@ -14,12 +16,31 @@ class Cors
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
-    {
-  
-        return $next($request)
-        -> header('Access-Control-Allow-Origin', "*")
-        -> header('Access-Control-Allow-Methods', "POST,PUT,GET,OPTIONS")
-        -> header('Access-Control-Allow-Headers', "Accept,Authorization,Content-Type,");
-    }
+    public function handle($request, Closure $next) {
+//        return $next($request);
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+
+        if ($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return \Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+             $response->headers->set($key, $value); 
+        return $response;
+            }
 }
+
+# File::  app\Http\Kernel.php
+# Add following line in `protected $middleware` Array.
+# \App\Http\Middleware\CORS::class
+
+# And following in `protected $routeMiddleware` Array
+# 'cors' => \App\Http\Middleware\CORS::class
