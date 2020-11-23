ef9eb5865c736cee3fb258435fc701d162c27b10
abusiraj84 <abusiraj84@gmail.com>
2020-11-23 17:58:06 +0300

    make it better

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,7 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'CORS'=> \App\Http\Middleware\CORS::class,
         'cors' => \Barryvdh\Cors\HandleCors::class
     ];
 }
